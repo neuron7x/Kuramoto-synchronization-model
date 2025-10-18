2ac89963b7648c1de5cab2c277a0ddd97cf40ea8
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-10-19 02:25:55 +0300

    Add high-efficiency training engine with profiling

--- core/neuro/__init__.py
+++ core/neuro/__init__.py
@@ -1,5 +1,34 @@
 """Neuroscience-inspired modules for TradePulse."""
 
 from .amm import AdaptiveMarketMind, AMMConfig
+from .training import (
+    AsyncDataLoader,
+    CheckpointManager,
+    MixedPrecisionContext,
+    ProfileSnapshot,
+    TrainingBatch,
+    TrainingComponent,
+    TrainingConfig,
+    TrainingEngine,
+    TrainingProfiler,
+    TrainingSample,
+    TrainingStepResult,
+    TrainingSummary,
+)
 
-__all__ = ["AMMConfig", "AdaptiveMarketMind"]
+__all__ = [
+    "AMMConfig",
+    "AdaptiveMarketMind",
+    "AsyncDataLoader",
+    "CheckpointManager",
+    "MixedPrecisionContext",
+    "ProfileSnapshot",
+    "TrainingBatch",
+    "TrainingComponent",
+    "TrainingConfig",
+    "TrainingEngine",
+    "TrainingProfiler",
+    "TrainingSample",
+    "TrainingStepResult",
+    "TrainingSummary",
+]
