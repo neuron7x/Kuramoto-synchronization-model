3fa731dc2f327d70edc397230bf7dcaed7a2cb81
Yaroslav Vasylenko <stapkabenderka@ukr.net>
2025-10-21 01:37:59 +0300

    Add integrated platform assembly utilities

---
+++ src/system/__init__.py
@@ -0,0 +1,13 @@
+"""High-level platform assembly helpers."""
+
+from .integration import (
+    StreamingPipelineSettings,
+    TradePulsePlatform,
+    build_tradepulse_platform,
+)
+
+__all__ = [
+    "StreamingPipelineSettings",
+    "TradePulsePlatform",
+    "build_tradepulse_platform",
+]
